e2c045b2bf95e883680b970ebff6dca4883d70ca
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-01-24 00:48:10 +0300

    Change

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,19 +3,10 @@ import base64
 from django.contrib.auth import get_user_model
 from django.core.files.base import ContentFile
 from djoser.serializers import UserSerializer
-from rest_framework import serializers
-
 from recipes.constants import RecipesModels
-from recipes.models import (
-    Favorite,
-    Ingredient,
-    Recipe,
-    RecipeIngredient,
-    RecipeTag,
-    ShoppingCart,
-    Tag,
-    Unit,
-)
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            RecipeTag, ShoppingCart, Tag, Unit)
+from rest_framework import serializers
 from users.models import Subscription
 
 User = get_user_model()
