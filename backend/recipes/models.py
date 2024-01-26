507fe96934657e0ef1ceeb59ddaa58f0b2f6a312
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-01-26 14:38:51 +0300

    Changes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,7 +2,6 @@ from colorfield.fields import ColorField
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
 from recipes.constants import RecipesModels
 
 User = get_user_model()
