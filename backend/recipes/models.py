27ee6b560f326d1e7b29740537c777da0a4f47f5
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-01-26 15:35:18 +0300

    Changes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,6 +2,7 @@ from colorfield.fields import ColorField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from users.models import User
+
 from recipes.constants import RecipesModels
 
 
